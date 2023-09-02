60e3576c0c89832c7e07eb926492d7bd569c858a
hirridwan <rmfhdofficial@gmail.com>
2023-09-02 17:42:28 +0700

    update format

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from app.config.database import create_connection
-from app.controller.ruangwa import RuangWa
+# from app.config.database import create_connection
+# from app.controller.ruangwa import RuangWa
 from app.controller.send_message import *
 from app.controller.send_image import *
 
